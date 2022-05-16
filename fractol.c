32c01f1cf5fca3242778707330d40a280de703e0
iarinov <95414938+iarinov@users.noreply.github.com>
2022-05-16 17:52:30 +0200

    Add files via upload

--- fractol.c
+++ fractol.c
@@ -6,7 +6,7 @@
 /*   By: aiarinov <aiarinov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/15 15:37:36 by aiarinov          #+#    #+#             */
-/*   Updated: 2022/05/16 16:23:30 by aiarinov         ###   ########.fr       */
+/*   Updated: 2022/05/16 17:50:25 by aiarinov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,34 +22,55 @@ int	main(void)
 	int		color;
 	int x;
 	int y;
+	int a;
 
 	mlx = mlx_init();
-	x = 100;
-	y = 100;
+	x = 250;
+	y = 250;
+	a = 1;
 	color = 913831;
 	mlx_win = mlx_new_window(mlx, 500, 500, "Hello world!");
-	while (x < 250 && y < 250)
-	{
-		mlx_pixel_put(mlx, mlx_win, x, y, color);
-		x++;
-		y++;
-	}
-	while (x < 400 && y > 100)
-	{
-		mlx_pixel_put(mlx, mlx_win, x, y, color);
-		x++;
-		y--;
-	}
-	while (x > 100)
-	{
-		mlx_pixel_put(mlx, mlx_win, x, y, color);
-		x--;
-	}
-	// while (y < 350)
+	// while (x < 250 && y < 250)
 	// {
 	// 	mlx_pixel_put(mlx, mlx_win, x, y, color);
+	// 	x++;
 	// 	y++;
 	// }
+	// while (x < 400 && y > 100)
+	// {
+	// 	mlx_pixel_put(mlx, mlx_win, x, y, color);
+	// 	x++;
+	// 	y--;
+	// }
+	// while (x > 100)
+	// {
+	// 	mlx_pixel_put(mlx, mlx_win, x, y, color);
+	// 	x--;
+	// }
+	// while (x <= 300 && y != 300)
+	// {
+	// 	mlx_pixel_put(mlx, mlx_win, x, y, color);
+	// 	x++;
+	// 	while (x == 300 && y <= 300)
+	// 	{
+	// 		mlx_pixel_put(mlx, mlx_win, x, y, color);
+	// 		y++;
+	// 	}
+	// }
+	// while (x >= 100 && y >= 100)
+	// {
+	// 	mlx_pixel_put(mlx, mlx_win, x, y, color);
+	// 	x--;
+	// 	y--;
+	// }
+
+	while (x != 100 && y != 100 && a != 360)
+	{
+		mlx_pixel_put(mlx, mlx_win, x, y, color);
+		x = 100 * sin(a);
+		y = 100 * cos(a);
+		a++;
+	}
 	mlx_loop(mlx);
 	mlx_destroy_window(mlx, mlx_win);
 	free(mlx);
